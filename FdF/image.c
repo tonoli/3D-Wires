bc8226bb37f3d27496011eb6e37e566b5d00da0d
itonoli <itonoli-@student.42.fr>
2017-03-24 16:27:45 +0100

    image.c tests and corrections

--- FdF/image.c
+++ FdF/image.c
@@ -14,26 +14,30 @@
 
 void	create_image(t_env *env)
 {
-	int		bpp;
-	int		size_line;
-	int		endian;
-
 	env->ptr_img = mlx_new_image(env -> mlx, WIDTH, HEIGHT);
-	env->img_str = mlx_get_data_addr(env->ptr_img, &bpp, &size_line, &endian);
+	env->img_str = mlx_get_data_addr(env->ptr_img, &env->bpp, &env->size_line, &env->endian;
 
 }
 
-void	fill_pixel(char *img_str, int x, int y, t_env *env)
+void	fill_pixel(int x, int y, t_env *env)
 {
-	img_str[4 * x + 4 * WIDTH * y] = env->blue;
-	img_str[4 * x + 4 * WIDTH * y + 1] = env->green;
-	img_str[4 * x + 4 * WIDTH * y + 2] = env->red;
+	env->img_str[(env->bpp / 8) * x + env->size_line * y] = mlx_get_color_value(env->mlx, BLUE);
+	env->img_str[(env->bpp / 8) * x + env->size_line * y + 1] = mlx_get_color_value(env->mlx, GREEN);
+	env->img_str[(env->bpp / 8) * x + env->size_line * y + 2] = mlx_get_color_value(env->mlx, RED);
 }
 
-void	fill_img(t_env *env)
+void	fill_img(t_env *env, int current_x)
 {
 	// Prendre l'image et allumer tous les points necessaire grace
 	// aux fonctions draw qui diront lesquels alluner  et fill_pixel qui
 	// viendra les allumer deriere
+	int pen = env->draw_start;
+	create_image(env);
+	while(count != env->draw_end)
+	{
+		put_pixel_to_img(env, current_x, pen);
+		mlx_put_image_to_window(env->mlx, env->win, env->img, 420, 420);
+		pen++;
+	}
 
 }
