10e87e8acea22e93f3b63169f8501394df72bb67
Ignacio Tonoli de maussion <itonoli-@e1z2r3p25.42.us.org>
2017-06-04 16:37:11 -0700

    3D in the sky

--- FdF/minilibx/test.sh
+++
@@ -1,8 +0,0 @@
-if [ ! -e '$HOME/goinfre' ] ; then
-	    mkdir /goinfre/$(whoami)
-	fi
-	export PATH="$HOME"/goinfre/brew/bin:"$PATH"
-	if [ ! -d '$HOME/goinfre/brew/bin' ] ; then
-		    git clone https://github.com/Homebrew/homebrew.git ~/goinfre/brew
-			    brew update
-			fi 
